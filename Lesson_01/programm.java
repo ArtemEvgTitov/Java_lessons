637b31257f7e57e627a4d422d41c53580b87ca11
ArtemEvgTitov <>
2022-10-17 12:06:41 +0300

    добавлен конспект 1 семинара

--- Lesson_01/programm.java
+++ Lesson_01/programm.java
@@ -1,5 +1,25 @@
 package Lesson_01;
+import java.util.Scanner; // для ввода в терминал
+/* Многострочные комментарии
+ * пишутся так
+ */
+public class programm { // однострочный комментарий
+    public static void main(String[] args) {
+        short age = 10;
+        int salary = 123_456; // Для больших значений можно разделять разряды нижним подчёркиванием
+        float e = 2.7f; // float ОБЯЗАТЕЛЬНО с суффиксом f
+        double pi = 3.1415; // Для double тоже есть суффикс - D. Но он не является обязательным
+        char ch = '}';
+        boolean flag = 123 < 234;
+        String msg = "Hello World";
+        int[] arr = new int[] {1, 2, 3};
 
-public class programm {
-    
+
+        Scanner iScanner = new Scanner(System.in); // работа с вводом в терминал
+        System.out.println("int a: ");
+        int x = iScanner.nextInt();
+        System.out.println(x);
+        iScanner.close();
+
+    }
 }
