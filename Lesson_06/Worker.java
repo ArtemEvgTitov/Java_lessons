0baac495c9341445f7eb21ef09630b13acabb256
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-06-07 22:25:12 +0300

    fix

--- Lesson_06/Worker.java
+++ Lesson_06/Worker.java
@@ -1,5 +1,14 @@
 package Lesson_06;
 
 public class Worker {
-    
+    int id;
+    int salary;
+    String firstName;
+    String lastName;
+
+    @Override
+    public String toString() {
+        String res = String.format("id: %d fn: %s ln: %s s: %d", id, firstName, lastName, salary);
+        return res;
+    }
 }
